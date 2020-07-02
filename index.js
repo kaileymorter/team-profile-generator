899b4ce65647b1f9170c0a9f530da0e8f62e9ec0
Spartacus <k_morter@outlook.com>
2020-07-02 15:25:55 -0600

    added profile-template

--- index.js
+++ index.js
@@ -1,61 +1,157 @@
- const inquirer = require('inquirer');
- const Employee = require('./lib/Employee');
- const Manager = require('./lib/Manager');
- const Engineer = require('./lib/Engineer');
- const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateProfile = require('./src/profile-template')
+const writeFile = require('./src/generate-site')
 
-const PromptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: "What is the employee's role",
-            choices: ['Manager', 'Engineer', 'Intern'],
-            validate: roleInput => {
-                if (roleInput) {
-                    return true;
-                } else {
-                    console.log('Please select a role!');
-                }
-            }
-        }, 
+const manager = [];
+const engineer = [];
+const intern = [];
+const employeeArr = {manager, engineer, intern}
+
+const promptUser = () => {
+    return inquirer.prompt([ 
         {
             type: 'text',
             name: 'name',
-            message: "What is the employee's name?",
+            message: "What is the manager's name?",
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log('Please enter a name for your employee!');
+                    console.log('Please enter a name for the Manager profile!');
                 }
             }
         },
         {
             type: 'text',
             name: 'id',
-            message: "What is the employee's ID number?",
+            message: "What is the manager's employee ID number?",
             validate: idInput => {
                 if (idInput) {
                     return true;
                 } else {
-                    console.log("Please give your employee's ID number");
+                    console.log("Please provide an employee ID number!");
                 }
             }
         },
         {
             type: 'text',
             name: 'email',
-            message: "What is the employee's email?",
+            message: "What is the manager's email?",
             validate: emailInput => {
                 if (emailInput) {
                     return true;
                 } else {
-                    console.log("Please provide an email for your employee's profile");
+                    console.log("Please provide an email for your manager's profile");
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'officeNumber',
+            message: "What is the manager's office number?",
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log("Please provide the manager's office number!");
                 }
             }
         }
     ])
+    .then(({name, id, email, officeNumber}) => {
+        manager.push(new Manager(name, id, email, officeNumber))
+    })
+};
+
+const promptProfile = (employeeArr) => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: "Would you like to add an Engineer, Intern or done with building your team profile?",
+            choices: ['Add Engineer!', 'Add Intern!', 'Finished with Team Profile!'],
+            validate: roleNumInput => {
+                if (roleNumInput) {
+                    return true;
+                } else {
+                    console.log("Please select an option!");
+                }
+            }
+        }
+    ])
+    .then(({employee}) => {
+        if (employee === 'Add Engineer!') {
+            return inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'name',
+                    message: "Enter Engineer's name.",
+                },
+                {
+                    type: 'text',
+                    name: 'id',
+                    message: "Enter Engineer's employee ID number."
+                },
+                {
+                    type: 'text',
+                    name: 'email',
+                    message: "Provide Engineer's work email address."
+                },
+                {
+                    type: 'text',
+                    name: 'github',
+                    message: "Provide Engineer's GitHub username for their team profile."
+                },
+            ])
+            .then(({name, id, email, github}) => {
+                engineer.push(new Engineer(name, id, email, github))
+                return promptProfile()
+            })
+        } else if (employee === 'Add Intern!') {
+            return inquirer.prompt([
+                {
+                    type: 'text',
+                    name: 'name',
+                    message: "Enter Intern's name."
+                },
+                {
+                    type: 'text',
+                    name: 'id',
+                    message: "Enter Intern's employee ID number."
+                },
+                {
+                    type: 'text',
+                    name: 'email',
+                    message: "Provide Intern's email address."
+                },
+                {
+                    type: 'text',
+                    name: 'school',
+                    message: "What school did or is the intern going to?"
+                },
+            ])
+            .then(({name, id, email, school}) => {
+                intern.push(new Intern(name, id, email, school))
+                return promptProfile()
+            })
+        } else {
+            console.log('View your team profile!')
+            return employeeArr
+        }
+    });
 }
 
-PromptUser();
+promptUser()
+    .then(promptProfile)
+    .then(teamProfileData => {
+        return generateProfile(employeeArr)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ src/profile-template.js
@@ -0,0 +1,68 @@
+//create employee profiles
+const generateProfile = teamProfileData => {
+    const manager = teamProfileData.manager.map(function (employee){
+        return `
+            <div class="card col-sm-3 m-1" style="width: 18rem;">
+                <div class="p-2">
+                    <h2>${employee.name}</h2>
+                    <h4>Manager</h4>
+                    <p>ID: ${employee.id}</p>
+                    <p>Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                    <p>Office Number: ${employee.officeNumber}</p>
+                </div> 
+            </div>
+        `
+    })
+    const engineer = teamProfileData.engineer.map(function (employee){
+        return `
+            <div class="card col-sm-3 m-1" style="width: 18rem;">
+                <div class="p-2">
+                    <h2>${employee.name}</h2>
+                    <h4>Engineer</h4>
+                    <p>ID: ${employee.id}</p>
+                    <p>Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                    <p> Github: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></p>
+                </div>
+            </div>
+        `
+    })
+    const intern = teamProfileData.intern.map(function (employee){
+        return `
+            <div class="card col-sm-3 m-1" style="width: 18rem;">
+                <div class="p-2">
+                    <h2>${employee.name}</h2>
+                    <h4>Intern</h4>
+                    <p>ID: ${employee.id}</p>
+                    <p>Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+                    <p>School: ${employee.school}</p>
+                </div>
+            </div>
+        `
+    })
+    return manager + engineer + intern
+}
+
+
+module.exports = templateData => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            <title>My Work Team</title>
+        </head>
+        <body class="bg-light">
+            <header>
+            <h1 class="text-center bg-primary text-light p-3">My Work Team</h1>
+            </header>
+            <main class="container">
+                <div class="row justify-content-center">
+                ${generateProfile(templateData)}
+                <div>
+            </main>
+        </body>
+        </html>    
+        `
+}
